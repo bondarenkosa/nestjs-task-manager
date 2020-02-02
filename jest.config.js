d6f245424e37da324496b95b5133fecdfb6ac560
Sergei Bondarenko <andbondsa@gmail.com>
2020-02-02 22:52:04 +0500

    fix: add jwt env vars to the jest config

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ process.env.TYPEORM_CONNECTION = 'sqlite';
 process.env.TYPEORM_DATABASE = ':memory:';
 process.env.TYPEORM_SYNCHRONIZE = 'true';
 process.env.TYPEORM_LOGGING = 'false';
+process.env.JWT_EXPIRES_IN = 3600;
+process.env.JWT_SECRET = 'aldk4i7y3mcnz20g2gk6nsh31,a9r5h1';
 
 module.exports = {
   moduleFileExtensions: [
